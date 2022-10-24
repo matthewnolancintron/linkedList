2740fda2ca2368eed394d7f50db44d31640c8f0c
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2022-10-24 13:36:30 -0400

    added feature to insert and remove nodes at specified indices

--- linkedList.js
+++ linkedList.js
@@ -37,6 +37,69 @@ const linkedListFactory = () =>{
         contains: (value) => list.some(node => node.value == value),
         find:(value) => list.findIndex(node => node.value == value),
         toString: ()=> list.map(node => node.value.toString()).join(' -> ') + ' -> null',
+        /**
+        Extra Credit Tip: When you insert or remove a node, consider how it will affect the existing nodes. Some of the nodes will need their nextNode link updated.
+         */
+        insertAt:(value,index)=>{
+            /**
+             insertAt(value, index) that inserts a new node with the provided value at the given index. 
+             */
+            if(index < list.length-1 && index > 0){
+                //node being inserted is not the head or the tail,
+                //set next node to the node that follows
+                list.splice(index,0,nodeFactory(value,list[index])); 
+
+                //since node being inserted isn't head or tail
+                // than there is a node before it who's nextNode value 
+                //needs to be updated
+
+                //previous node's nextnode value being updated
+                list[index-1].nextNode = list[index];
+                
+            } else if(index === 0){
+                //node being inserted as the head,
+                //set nextNode to node that follows
+                list.splice(index,0,nodeFactory(value,list[index])); 
+            } else if(index === list.length-1){
+                //node being insserted as the tail
+                list.push(nodeFactory(value)); 
+        
+                //only need to update the nextNode value for the node before 
+                list[list.length-2].nextNode = list[index+1];
+            }
+
+        },
+        removeAt:(index)=>{
+            /**
+             removeAt(index) that removes the node at the given index.
+             */
+            if(index === 0){
+                //if index is zero node being removed is the head
+                list.shift();
+
+                //update the nextNode value for the new head
+                list[index].nextNode = list[index+1];
+
+            } else if(index === list.length-1){
+                //if index is equal to last index of the list or list.length-1
+                //node being removed is the tail
+                list.pop();
+
+                //set nextNode value for new tail to null
+                list[index-1].nextNode = null;
+
+            } else {
+                //node being remove isn't the tail or the head
+                list.splice(index,1);
+
+                //need to update next node value for the node that was before the removed node
+                list[index-1].nextNode = list[index];
+            }
+
+
+            
+        }
+
     };
 }
 
--- linkedList.test.js
+++ linkedList.test.js
@@ -47,6 +47,22 @@ console.log(linkedList.find(10));
 //
 
 //
+console.log(linkedList.insertAt(7,0));
+console.log(linkedList.insertAt(3,2));
+console.log(linkedList.insertAt(2,3));
+console.log(linkedList.linkedList);
+//
+
+
+//
+console.log(linkedList.removeAt(0));
+console.log(linkedList.removeAt(3));
+console.log(linkedList.removeAt(1));
+console.log(linkedList.linkedList);
+
+//
+
+//
 console.log(linkedList.toString());
 //
 
